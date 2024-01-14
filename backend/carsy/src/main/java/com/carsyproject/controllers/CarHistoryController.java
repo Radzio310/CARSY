a2e1d1b22602e28d205f27d6dd30a3bd198f7ed6
KT <kamil.tajak@edu.uekat.pl>
2024-01-14 14:32:24 +0100

    Appointment delete, put func

--- backend/carsy/src/main/java/com/carsyproject/controllers/CarHistoryController.java
+++ backend/carsy/src/main/java/com/carsyproject/controllers/CarHistoryController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -48,26 +49,22 @@ public class CarHistoryController {
     @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     @Operation(summary = "Add a car history", description = "Add a new car history. Requires MODERATOR or ADMIN role.")
     public CarHistory addCarHistory(@RequestBody Map<String, Object> payload) throws ParseException {
-        // Parse the date from the payload
+
         Date date = new SimpleDateFormat("yyyy-MM-dd").parse((String) payload.get("date"));
 
-        // Get the serviceTypeId and carId from the payload
         Long serviceTypeId = Long.valueOf((Integer) payload.get("serviceTypeId"));
         Long carId = Long.valueOf((Integer) payload.get("carId"));
 
-        // Find the ServiceType and Car entities with the given IDs
         ServiceType serviceType = serviceTypeRepository.findById(serviceTypeId)
                 .orElseThrow(() -> new RuntimeException("Error: ServiceType is not found."));
         Car car = carRepository.findById(carId)
                 .orElseThrow(() -> new RuntimeException("Error: Car is not found."));
 
-        // Create a new CarHistory entity and set its fields
         CarHistory carHistory = new CarHistory();
         carHistory.setDate(date);
         carHistory.setServiceType(serviceType);
         carHistory.setCar(car);
 
-        // Save the CarHistory entity and return it
         return carHistoryRepository.save(carHistory);
     }
 
@@ -75,22 +72,19 @@ public class CarHistoryController {
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @Operation(summary = "Get car history", description = "Get car history for a specific car. Requires USER, MODERATOR, or ADMIN role.")
     public List<CarHistory> getMyCarHistory(@PathVariable Long carId) {
-        // Get the logged in user's ID
+
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Long userId = userDetails.getId();
 
-        // Check if the logged in user is a moderator or admin
         boolean isModeratorOrAdmin = userDetails.getAuthorities().stream()
                 .anyMatch(role -> role.getAuthority().equals("ROLE_MODERATOR") || role.getAuthority().equals("ROLE_ADMIN"));
 
-        // Check if the car with the given ID is owned by the logged in user
         Car car = carRepository.findById(carId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Car not found"));
         if (!car.getUser().getId().equals(userId) && !isModeratorOrAdmin) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have permission to view this car's history");
         }
 
-        // Return the history of the car
         return carHistoryRepository.findByCar(car);
     }
 
@@ -102,11 +96,23 @@ public class CarHistoryController {
         dateFormat.setLenient(false);
         Date date = dateFormat.parse((String) payload.get("date"));
 
+
+        Date currentDate = new Date();
+        if (date.before(currentDate)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Appointment date cannot be in the past");
+        }
+
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
         if (hour < 7 || hour > 17) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Appointments can only be scheduled between 7:00 and 17:00");
+        }
+
+        if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Appointments cannot be scheduled on weekends");
         }
 
         if (carHistoryRepository.findByDate(date).isPresent()) {
@@ -115,16 +121,96 @@ public class CarHistoryController {
 
         Long serviceTypeId = Long.valueOf((Integer) payload.get("serviceTypeId"));
         Long carId = Long.valueOf((Integer) payload.get("carId"));
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getId();
 
         ServiceType serviceType = serviceTypeRepository.findById(serviceTypeId)
                 .orElseThrow(() -> new RuntimeException("Error: ServiceType is not found."));
         Car car = carRepository.findById(carId)
                 .orElseThrow(() -> new RuntimeException("Error: Car is not found."));
 
+        if (!car.getUser().getId().equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have permission to schedule an appointment for this car");
+        }
+
         CarHistory carHistory = new CarHistory();
         carHistory.setDate(date);
         carHistory.setServiceType(serviceType);
         carHistory.setCar(car);
+
+        return carHistoryRepository.save(carHistory);
+    }
+
+    @DeleteMapping("/appointment/delete/{appointmentId}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    @Operation(summary = "Delete an appointment", description = "Delete an appointment. Requires USER, MODERATOR, or ADMIN role.")
+    public ResponseEntity<?> deleteAppointment(@PathVariable Long appointmentId) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getId();
+
+        CarHistory carHistory = carHistoryRepository.findById(appointmentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Appointment not found"));
+
+        if (carHistory.getDate().before(new Date())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot delete past appointments");
+        }
+
+        if (!carHistory.getCar().getUser().getId().equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have permission to delete this appointment");
+        }
+
+        carHistoryRepository.delete(carHistory);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/appointment/edit/{appointmentId}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    @Operation(summary = "Edit an appointment", description = "Edit an appointment. Requires USER, MODERATOR, or ADMIN role.")
+    public CarHistory editAppointment(@PathVariable Long appointmentId, @RequestBody Map<String, Object> payload) throws ParseException {
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getId();
+
+        CarHistory carHistory = carHistoryRepository.findById(appointmentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Appointment not found"));
+
+        if (!carHistory.getCar().getUser().getId().equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have permission to edit this appointment");
+        }
+
+        if (payload.containsKey("date")) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH");
+            dateFormat.setLenient(false);
+            Date date = dateFormat.parse((String) payload.get("date"));
+
+            if (carHistory.getDate().before(new Date())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot edit past appointments");
+            }
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            if (hour < 7 || hour > 17) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Appointments can only be scheduled between 7:00 and 17:00");
+            }
+
+            if (carHistoryRepository.findByDate(date).isPresent() && !carHistoryRepository.findByDate(date).get().getServiceID().equals(appointmentId)) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Date is already taken");
+            }
+
+            carHistory.setDate(date);
+        }
+
+        if (payload.containsKey("serviceTypeId")) {
+            // Get the serviceTypeId from the payload
+            Long serviceTypeId = Long.valueOf((Integer) payload.get("serviceTypeId"));
+
+            ServiceType serviceType = serviceTypeRepository.findById(serviceTypeId)
+                    .orElseThrow(() -> new RuntimeException("Error: ServiceType is not found."));
+
+            carHistory.setServiceType(serviceType);
+        }
 
         return carHistoryRepository.save(carHistory);
     }
